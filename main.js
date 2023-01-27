9c0fa3d066c1dbf68c4f47513d41a20970b314b8
Érica da Silva <114163982+ericamachads@users.noreply.github.com>
2023-01-27 15:48:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+var arrayAlunos = [];
+
+function enviar() {
+    //var nome1 = document.getElementById("aluno1").value;
+    //var nome2 = document.getElementById("aluno2").value;
+    //var nome3 = document.getElementById("aluno3").value;
+    // var nome4 = document.getElementById("aluno4").value;
+
+    // arrayAlunos.push(nome1);
+    // arrayAlunos.push(nome2);
+    // arrayAlunos.push(nome3);
+    // arrayAlunos.push(nome4);
+
+    for (var cont=1; cont<=4; cont++) {
+        var nome;
+        nome = document.getElementById("aluno" + cont).value;
+        console.log(nome);
+        arrayAlunos.push(nome);
+    }
+
+    console.log(arrayAlunos);
+    
+    escreveHTML();
+    document.getElementById("btnEnviar").style.display = "none";
+    document.getElementById("btnOrganizar").style.display = "inline-block";
+}
+
+function organizar() {
+    arrayAlunos.sort();
+    console.log(arrayAlunos);
+
+    // document.getElementById("mostraNomes").innerHTML = arrayAlunos;
+    escreveHTML();
+}
+
+function escreveHTML() {
+    var tamanhoArray = arrayAlunos.length;
+    var arrayHTML = [];
+    var html = "";
+
+    for (var i=0; i<tamanhoArray; i++) {
+        arrayHTML.push("<h4>Nome - " + arrayAlunos[i] + "</h4>");
+    }
+
+    console.log(arrayHTML);
+
+    html = arrayHTML.join("");
+    console.log(html);
+
+    document.getElementById("mostraNomes").innerHTML = html;
+}
